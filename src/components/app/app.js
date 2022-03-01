c45cda63daa554584891d8eda0d70df8d578aa94
rddomino <seltos@bk.ru>
2022-03-01 11:22:14 +0600

    Реализация лайков и нравиться через класс

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,15 +8,22 @@ import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form";
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'qwer'},
+        {label: 'That is good', important: false, id: 'asdf'},
+        {label: 'I need a break', important: false, id: 'zxcv'}
+    ];
+
     return (
         <div className="app">
-            <AppHeader/>
+            <AppHeader/>   
+            <PostAddForm/>         
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
+            </div>            
+            <PostList posts={data}/>            
         </div>
     )
 }
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,23 +1,59 @@
-import React from "react";
+import React, {Component} from "react";
 import './post-list-item.css';
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-star btn-sm" type="button">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button className="btn-trash btn-sm" type="button">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
-            </div>
-        </li>
-    )
-}
+export default class PostListItem extends Component {
 
-export default PostListItem;
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,            
+            like: false
+        };       
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);  
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+
+        let classNames = 'app-list-item d-flex justify-content-between';
+        const {label} = this.props;
+        const {important, like} = this.state;
+
+        if (important) {
+            classNames += ' important';
+        }
+
+        if (like) {
+            classNames += ' like';
+        }
+
+        return (
+            <div className={classNames}>
+                <span className="app-list-item-label" onClick={this.onLike}>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button className="btn-star btn-sm" type="button" onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button className="btn-trash btn-sm" type="button">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
+            </div>
+        )
+    }
+}
