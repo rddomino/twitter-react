6430acb9041cb9f2e5f4a02dc6153b22a9e4d40a
rddomino <seltos@bk.ru>
2022-03-02 13:21:53 +0600

    Реализация лайков и понравилось в главном компоненте с пробросом в дочернии

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,33 +3,10 @@ import './post-list-item.css';
 
 export default class PostListItem extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,            
-            like: false
-        };       
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);  
-    }
-
-    onImportant() {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
     render() {
-
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
-        const {label, onDelete} = this.props;
-        const {important, like} = this.state;
+        
 
         if (important) {
             classNames += ' important';
@@ -41,11 +18,11 @@ export default class PostListItem extends Component {
 
         return (
             <div className={classNames}>
-                <span className="app-list-item-label" onClick={this.onLike}>
+                <span className="app-list-item-label" onClick={onToggleLiked}>
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
-                    <button className="btn-star btn-sm" type="button" onClick={this.onImportant}>
+                    <button className="btn-star btn-sm" type="button" onClick={onToggleImportant}>
                         <i className="fa fa-star"></i>
                     </button>
                     <button className="btn-trash btn-sm" type="button" onClick={onDelete}>
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,13 +2,13 @@ import React from "react";
 import PostListItem from "../post-list-item/post-list-item";
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} onDelete={(() => onDelete(id))}/>
+                <PostListItem {...itemProps} onDelete={(() => onDelete(id))} onToggleImportant={() => onToggleImportant(id)} onToggleLiked={() => onToggleLiked(id)}/>
             </li>
         )
     });
